c29b13de91e59409afe35ee1eaf6d888e032cb05
TanKARASU <karasutan@gmail.com>
2021-01-25 17:19:35 +0100

    début de reporting des routes API

---
+++ index.js
@@ -0,0 +1,8 @@
+// importation et configuration de dotenv, afin d'acceder aux données sensibles
+// dans la variable d'environnement
+require("dotenv").config();
+
+// destructuration des variables d'environnement
+let { API_KEY, PORT } = process.env
+
+
