7d31df08343f892a61d5eceaa9fb6cb76fbd8536
TanKARASU <karasutan@gmail.com>
2021-01-25 19:38:22 +0100

    commit de merge

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,6 @@
 // GET //
 // route pour films populaires
-https: `//api.themoviedb.org/3/movie/popular?api_key=d147fe4e04ba78d12adf57c89fb3ad72&language=fr-FR&page=1`;
+ `https://api.themoviedb.org/3/movie/popular?api_key=d147fe4e04ba78d12adf57c89fb3ad72&language=fr-FR&page=1`;
 
 // POST //
 
