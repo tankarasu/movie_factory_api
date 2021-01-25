984e1d8b2bbd10e9eb50ff43c0c926c6a2219fe2
TanKARASU <karasutan@gmail.com>
2021-01-25 21:58:11 +0100

    probleme non réglé avec api & fetch&
    &

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // importation et configuration de dotenv, afin d'acceder aux données sensibles
 // dans la variable d'environnement
 require("dotenv").config();
-
 // destructuration des variables d'environnement
-let { API_KEY, PORT } = process.env
+let { API_KEY, PORT } = process.env;
 
+// config expressJS
+const express = require("express");
+const app = express();
 
+// routes
+const movieRouter = require("./routes/movieRouter.js");
+app.use('/',movieRouter)
+
+// listening
+app.listen(PORT, () => console.log("listening on port:" + PORT));
---
+++ routes/movieRouter.js
@@ -0,0 +1,69 @@
+// import des modules
+const express = require("express");
+let movieRouter = express.Router();
+const axios = require("axios");
+
+// variables globales
+const fetch = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
+
+movieRouter.route("/").get((req, res) => {
+  apiCall("/movie/popular", "get", {}, res, result => {
+    console.log(result);
+  });
+});
+
+// GET //
+// route pour films populaires
+// `https://api.themoviedb.org/3/movie/popular?api_key=d147fe4e04ba78d12adf57c89fb3ad72&language=fr-FR&page=1`;
+// route pour correspondance id et nom de catégories
+// `https://api.themoviedb.org/3/genre/movie/list?api_key=d147fe4e04ba78d12adf57c89fb3ad72&language=en-US`;
+
+// route principale?
+//`https://api.themoviedb.org/3/discover/movie?api_key=d147fe4e04ba78d12adf57c89fb3ad72`;
+
+// route par catégories
+//`https://api.themoviedb.org/3/discover/movie?api_key=d147fe4e04ba78d12adf57c89fb3ad72&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28`;
+
+// route par année
+//`https://api.themoviedb.org/3/discover/movie?api_key=d147fe4e04ba78d12adf57c89fb3ad72&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&year=2020` // route recherche par titre de film
+//`https://api.themoviedb.org/3/search/movie?api_key=d147fe4e04ba78d12adf57c89fb3ad72&language=en-US&page=1&include_adult=false&query=bird`;
+
+// route recherche par personnes (acteurs ET réalisateurs, etc)
+//`https://api.themoviedb.org/3/search/person?api_key=d147fe4e04ba78d12adf57c89fb3ad72&language=en-US&query=pitt&page=1&include_adult=false`;
+
+// Détails acteurs pour chaque film
+//`https://api.themoviedb.org/3/movie/405774/credits?language=en-US&api_key=d147fe4e04ba78d12adf57c89fb3ad72`;
+
+// préfixe pour images (poster path obtenu dans les détails de chaque film)
+//`https://image.tmdb.org/t/p/w500/`;
+
+// POST //
+
+// PUT //
+
+// DELETE //
+
+// export le router
+console.log(process.env.API_KEY);
+module.exports = movieRouter;
+
+// functions
+function apiCall(url, method, data, res, next) {
+  fetch({
+    method: method,
+    url: url,
+    data: data,
+  })
+    .then(response => {
+      if (response.data.status == "success") {
+        next(response.data.result);
+      } else {
+        res.send("request failed");
+      }
+    })
+    .catch(err => {
+      res.send("request failed");
+    });
+}
