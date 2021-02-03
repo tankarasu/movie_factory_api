61318da7d88213f9f7f90709d3e80fccc3e93e5e
TanKARASU <karasutan@gmail.com>
2021-02-03 09:37:07 +0100

    petit correctif cors

--- index.js
+++ index.js
@@ -13,8 +13,17 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 // cors
 const cors = require("cors");
-app.options("*", cors());
-app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"] }));
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 mongoose.connect(mongoURI, {
   useUnifiedTopology: true,
