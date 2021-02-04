513d46e117a52af623d31d6d364a27bc8f6abfbf
Yegabebar <m.barbegayet@gmail.com>
2021-02-04 11:48:26 +0100

    Modification route forgot et ajout route update user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -107,88 +107,111 @@ userRouter.post("/signup", (req, res) => {
 userRouter.post("/forgot", (req, res) => {
   let users = [];
   User.find({})
-    .exec()
-    .then(item => {
-      let users = [];
-      // utilisation de la mÃ©thode find de mongodb pour parcourir la table user, sans paramÃ¨tres de recherche (d'oÃ¹ les parenthÃ¨ses vides)
-      User.find({})
-        .exec()
-        .then(item => {
-          // destructuring par spread operator pour ajout de chaque entrÃ©e de la table user dans le tableau users
-          users = [...item];
-          let found = false;
-          const regex = new RegExp( // initialisation regex pour vÃ©rification format d'adresse email
-            "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+[.][a-zA-Z0-9-.]+$"
-          );
-          // si la regex matche bien l'adresse email passÃ©e en requÃªte
-          if (regex.test(req.body.recipientEmail)) {
-            users.forEach(element => {
-              // on itÃ¨re sur la table users pour trouver un user qui utilise cet email
-              if (element.email == req.body.recipientEmail) {
-                // si l'adresse email est trouvÃ©e en base, on va pouvoir dÃ©clencher l'envoi de mail
-                found = true;
+  .exec()
+  .then(item => {
+    let users = [];
+    // utilisation de la méthode find de mongodb pour parcourir la table user, sans paramètres de recherche (d'où les parenthèses vides)
+    User.find({})
+      .exec()
+      .then(item => {
+        // destructuring par spread operator pour ajout de chaque entrée de la table user dans le tableau users
+        users = [...item];
+        let found = false;
+        const regex = new RegExp( // initialisation regex pour vérification format d'adresse email
+          "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+[.][a-zA-Z0-9-.]+$"
+        );
+        // si la regex matche bien l'adresse email passée en requête
+        if (regex.test(req.body.recipientEmail)) {
+          users.forEach(element => {
+            // on itère sur la table users pour trouver un user qui utilise cet email
+            if (element.email == req.body.recipientEmail) {
+              // si l'adresse email est trouvée en base, on va pouvoir déclencher l'envoi de mail
+              found = true;
+            }
+          });
+          if (found) {
+            // partie envoi de mail
+            const nodemailer = require('nodemailer');
+            const {smtpEmail,smtpPassword,oAuthID,oAuthSecret,refreshToken } = process.env;
+            // création d'un objet transporter
+            let transporter = nodemailer.createTransport({
+              service: 'gmail',
+              auth: {
+                type: 'OAuth2',
+                user: smtpEmail,
+                pass: smtpPassword,
+                clientId: oAuthID,
+                clientSecret: oAuthSecret,
+                refreshToken: refreshToken
+              }
+            });  
+              let mailOptions = {
+              from: "'The Movie Factory team' <tmfresetservice@gmail.com>",
+              to: req.body.recipientEmail,
+              subject: 'Password reset link',
+              html: `<p>Email reset asked for  ${req.body.recipientEmail}</p>
+                      <a target="_blank" href="https://tankarasu.github.io/movie_factory/reset/${req.body.recipientEmail}">
+                        <button id="button-ish">Reset Password</button>
+                      </a>`
+            };
+            
+            transporter.sendMail(mailOptions, function(err, data) {
+              if (err) {
+                console.log(timeStamp()+" Sending failed");
+                res.send("failed");
+              } else {
+                console.log(timeStamp()+" Email sent successfully");
+                res.send("success");
               }
             });
-            if (found) {
-              // partie envoi de mail
-              const nodemailer = require("nodemailer");
-              const {
-                smtpEmail,
-                smtpPassword,
-                oAuthID,
-                oAuthSecret,
-                refreshToken,
-              } = process.env;
-              // crÃ©ation d'un objet transporter
-              let transporter = nodemailer.createTransport({
-                service: "gmail",
-                auth: {
-                  type: "OAuth2",
-                  user: smtpEmail,
-                  pass: smtpPassword,
-                  clientId: oAuthID,
-                  clientSecret: oAuthSecret,
-                  refreshToken: refreshToken,
-                },
-              });
-              let mailOptions = {
-                from: "'The Movie Factory team' <tmfresetservice@gmail.com>",
-                to: req.body.recipientEmail,
-                subject: "Password reset link",
-                html: `<p>Email reset asked for  ${req.body.recipientEmail}</p>
-                      <a target="_blank" href="http://127.0.0.1:3051/reset/${req.body.recipientEmail}">
-                        <button id="button-ish">Reset Password</button>
-                      </a>`,
-              };
-
-              transporter.sendMail(mailOptions, function (err, data) {
-                if (err) {
-                  console.log(timeStamp() + " Sending failed");
-                  res.send("failed");
-                } else {
-                  console.log(timeStamp() + " Email sent successfully");
-                  res.send("success");
-                }
-              });
-            } else {
-              // sinon si found est toujours Ã  false, adresse non trouvÃ©e en base
-              console.log(timeStamp() + " User not found: " + req.body.email);
-              res.send("not found");
-            }
           } else {
-            // si recipientEmail ne matche pas la regex, renvoie invalid
-            console.log(
-              timeStamp() + " Invalid email syntax: " + req.body.email
-            );
-            res.send("invalid");
+            // sinon si found est toujours à false, adresse non trouvée en base
+            console.log(timeStamp()+" User not found: "+req.body.email);
+            res.send("not found");
           }
-        });
+        } else {
+          // si recipientEmail ne matche pas la regex, renvoie invalid
+          console.log(timeStamp()+" Invalid email syntax: "+req.body.email);
+          res.send("invalid");
+        }
+        
+
     });
+  });
 });
 
-userRouter.put("/update-user/:email", (req, res) => {
-  res.send("votre mot de passe a été changé");
-  // TODO à creuser
+userRouter.post("/update-user", (req, resp) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    // s'il n'existe pas d'utilisateur avec cette adresse email, avertissement
+    if (!user) {
+      resp.status(404).json({ error: "No user with that email found" });
+      console.log(timeStamp()+" User not found: "+req.body.email);
+      resp.send("not found");
+    }
+    else {
+      bcrypt.hash(req.body.password, rounds, (error, hash) => {
+        if (error) {
+          resp.status(500).json(error);
+          console.log(timeStamp()+" Password hash failed");
+          resp.send("failed");
+        } else {
+            var newPassword = {$set: {password: hash} };
+            User.updateOne(user, newPassword, function(err, res) {
+              if(err){
+                console.log(timeStamp()+" Update failed: "+req.body.email);
+                resp.send("failed");
+              }else{
+                console.log(timeStamp()+" Updated: "+req.body.email);
+                resp.send("success");
+              }
+            });
+        }
+      });
+      
+    }
+  });
+
+
 });
 
 userRouter.put("/addfavorite", async (req, res, next) => {
@@ -258,4 +281,9 @@ function generateToken(user) {
     tokenSecret,
     { expiresIn: "24h" }
   );
+}
+
+function timeStamp() {
+  let ts = new Date();
+  return `${ts.getUTCDay()}-${ts.getUTCMonth()}-${ts.getUTCFullYear()}-${ts.getHours()}:${ts.getMinutes()}:${ts.getSeconds()}`;
 }
